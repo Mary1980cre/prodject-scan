5f91edf64af3cacdc48208d2c46233edba9eae2b
Mary1980cre <77752505+Mary1980cre@users.noreply.github.com>
2023-10-29 23:54:29 +0300

    Whyus.jsx

---
+++ src/components/sections/Whyus.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+function Whyus() {
+  return (
+
+    <div class="container whyus">
+    <h2>
+        Почему именно мы
+    </h2>
+
+    <div id="outer">
+    <div id="inner">
+
+        <div class="caruseul-cust"> 
+        <img src="./img/Maskgroup.svg" alt="logo" />
+            <p class="card-text">Высокая и оперативная <br/>скорость обработки <br/> заявки</p>
+        </div>
+
+        <div class="caruseul-cust">
+        <img src="./img/Mask group (1).svg" alt="logo" />
+        <p class="card-text">Огромная комплексная база данных, обеспечивающая объективный ответ на запрос</p>
+        </div>
+
+        <div class="caruseul-cust">
+        <img src="./img/Mask group (2).svg" alt="logo" />
+            <p class="card-text">Защита конфеденциальных сведений, не подлежащих разглашению по федеральному </p>
+        </div>
+
+        <div class="caruseul-cust">
+        <img src="./img/Mask group (1).svg" alt="logo" />
+        <p class="card-text">Огромная комплексная база данных, обеспечивающая объективный ответ на запрос</p>
+        </div>
+
+        <div class="caruseul-cust">
+        <img src="./img/Mask group (1).svg" alt="logo" />
+        <p class="card-text">Огромная комплексная база данных, обеспечивающая объективный ответ на запрос</p>
+        </div>
+
+        <div class="caruseul-cust">
+        <img src="./img/Mask group (1).svg" alt="logo" />
+        <p class="card-text">Огромная комплексная база данных, обеспечивающая объективный ответ на запрос</p>
+        </div>
+     
+</div>
+</div>
+  <input type="button" id="left-button" class="leftArrow" value=""/>
+  <input type="button" id="right-button" class="rightArrow" value=""/>
+
+
+
+
+
+   
+         
+
+<div class="container">
+<img src="./img/Group 14.png" class="img-fluid image" alt="image" />
+</div>
+</div>
+
+
+
+
+
+  );
+}
+
+export default Whyus;
